2a80aa0ebff5fe49470039463cd4e1cc9fc81d95
sUeharaE4 <funa1hasi2hasi@gmail.com>
2022-01-02 19:28:17 +0900

    Update: refactor for resucing code complexity

--- src/common/util.py
+++ src/common/util.py
@@ -1,4 +1,4 @@
-"""Provide utility functions"""
+"""Provide utility functions."""
 from typing import Any, Union
 
 
@@ -8,13 +8,18 @@ def flatten_dict_in_dict(base_dict: dict[str, Union[str, dict[str, Any]]]):
 
     Example: {'author': {'id': 1, 'name': 'name', ...}} -> {'author-id': 1, 'author-name': 'name'...}
     """
-    drop_keys = []
-    add_dict = dict()
+
+    drop_keys: list[str] = []
+    add_dict: dict[str, Any] = dict()
     for base_dict_key, base_dict_value in base_dict.items():
-        if isinstance(base_dict_value, dict):
-            drop_keys.append(base_dict_key)
-            for k, v in base_dict_value.items():
-                add_dict[f"{base_dict_key}-{k}"] = v
+        __flatten_dict(base_dict_key, base_dict_value, drop_keys, add_dict)
     for drop_key in drop_keys:
         del base_dict[drop_key]
     base_dict.update(add_dict)
+
+
+def __flatten_dict(key: str, value: Any, drop_keys: list[str], add_dict: dict[str, Any]):
+    if isinstance(key, dict):
+        drop_keys.append(key)
+        for k, v in value.items():
+            add_dict[f"{key}-{k}"] = v
