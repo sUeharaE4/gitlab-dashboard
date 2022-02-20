c61e0c69d22259e1b72e0efac79a5cb6d85481de
sUeharaE4 <funa1hasi2hasi@gmail.com>
2022-02-20 21:32:44 +0900

    Add: create amount of changes view

--- src/view/mergerequest.py
+++ src/view/mergerequest.py
@@ -43,6 +43,7 @@ def create_mergerequest_view(group_id: int):
 
     create_count_of_created_mr_view(filtered_df)
     create_size_view(filtered_df)
+    create_changed_amount_view(filtered_df)
 
     st.markdown("## Detail")
     util.show_table("All of MergeRequests", df)
@@ -88,6 +89,53 @@ def create_size_view(mergerequest_df: pd.DataFrame):
             alt.Y("mean_change_files", title="mean change files(sum of change files / commits)"),
             color="project_name",
             size=alt.Size("user_notes_count", bin=True),
+            tooltip=tooltip,
+        )
+    ).interactive()
+    st.altair_chart(chart, use_container_width=True)
+
+
+def create_changed_amount_view(merge_request_df: pd.DataFrame):
+    """Create chart to show how many changes in the repository."""
+    st.markdown("## Amount of changes.")
+    st.markdown(
+        "Check how much of each file has been modified and the number of modifications. "
+        "Files that are changed frequently and in large amounts may have too much responsibility."
+    )
+    df = merge_request_df.copy()
+
+    target_projects = sorted(set(df["project_name"]))
+    view_target_project = st.selectbox("Select target project you want to see graphs", target_projects, 0)
+    df = df[df["project_name"] == view_target_project]
+    total_diffs = dict()
+    for commit_diff in df["diff"].to_list():
+        for file_path, diff in commit_diff.items():
+            if file_path not in total_diffs:
+                total_diffs[file_path] = dict()
+            total_diffs[file_path]["add"] = total_diffs[file_path].get("add", 0) + diff["add"]
+            total_diffs[file_path]["del"] = total_diffs[file_path].get("del", 0) + diff["del"]
+            total_diffs[file_path]["change_cnt"] = total_diffs[file_path].get("change_cnt", 0) + diff["change_cnt"]
+    diff_df = pd.DataFrame(
+        [
+            {"file_path": k, "add": v["add"], "del": v["del"], "change_cnt": v["change_cnt"]}
+            for k, v in total_diffs.items()
+        ]
+    )
+    diff_df["total_changes"] = diff_df["add"] + diff_df["del"]
+    diff_df["add/del"] = diff_df["add"] / (diff_df["del"] + 1)
+    diff_df = diff_df.sort_values("total_changes", ascending=False).reset_index()
+
+    top_n = st.slider("Set top N files changes a lot of lines", 1, diff_df.shape[0], min(1000, diff_df.shape[0]))
+    diff_df = diff_df[diff_df.index < top_n]
+
+    tooltip = diff_df.columns.tolist()
+    chart = (
+        alt.Chart(diff_df)
+        .mark_point()
+        .encode(
+            alt.X("change_cnt", title="change times"),
+            alt.Y("total_changes", title="total changes"),
+            size="add/del",
             tooltip=tooltip,
         )
     ).interactive()
