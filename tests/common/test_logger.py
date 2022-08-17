f71754ac007430ae69b57ff915c61ae412a4b314
sUeharaE4 <funa1hasi2hasi@gmail.com>
2022-08-17 22:28:13 +0900

    Add: create test scripts for logger

---
+++ tests/common/test_logger.py
@@ -0,0 +1,32 @@
+import logging
+import shutil
+from pathlib import Path
+
+import pytest
+
+from common.Logger import CustomLogger, get_logger
+
+
+@pytest.fixture()
+def clean_log_dir():
+    log_dir = Path(__file__).parents[0].joinpath("test_logs")
+    yield log_dir
+    shutil.rmtree(str(log_dir))
+
+
+def test_get_logger(clean_log_dir):
+    test_log_dir = clean_log_dir
+    logger = get_logger(name=__file__, log_path=test_log_dir.joinpath("test.log"))
+    assert len(logger.handlers) == 2
+    assert isinstance(logger.handlers[1], logging.FileHandler)
+    assert logger == get_logger(name=__file__)
+    assert logger != get_logger(test_log_dir)
+
+
+def test_logging():
+    logger = get_logger()
+    logger.debug("debug")
+    logger.info("info")
+    logger.warning("warning")
+    logger.error("error")
+    logger.critical("critical")
